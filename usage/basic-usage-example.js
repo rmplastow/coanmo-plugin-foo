a1533300d5c9fa4918d62e9a047ca39b08d13b83
Rich Plastow <rich.plastow@easeltv.com>
2020-07-04 08:00:10 +0100

    Adds index.html, which uses the usage example

--- usage/basic-usage-example.js
+++ usage/basic-usage-example.js
@@ -1,7 +1,22 @@
-// Run this in Node:
-// $ node --experimental-modules ./usage/basic-usage-example.js
+/**
+ * To run in Node:
+ * ```bash
+ * node --experimental-modules ./usage/basic-usage-example.js
+ * ```
+ * 
+ * To run in a browser:
+ * ```html
+ * <div id="app"></div>
+ * <script src="./usage/basic-usage-example.js" type="module"></script>
+ * ```
+ */
 
 import { CoAnMoPluginFoo } from '../dist/coanmo-plugin-foo.js';
 
-const foo = new CoAnMoPluginFoo();
+const foo = new CoAnMoPluginFoo('Basic Usage Example');
 console.log('foo.getFoo():', foo.getFoo());
+
+if (typeof document === 'object') {
+  const $app = document.getElementById('app');
+  if ($app) $app.innerHTML += `foo.getFoo(): ${foo.getFoo()}`;
+}
